6094500a130c28bf024cda41382d4aa3a2972c1b
AllyssonEstadulho92 <163911342+AllyssonEstadulho92@users.noreply.github.com>
2026-09-02 20:42:17 +0100

    fix: registar service worker v38

--- events.js
+++ events.js
@@ -376,7 +376,7 @@ async function enterApp() {
   if ('serviceWorker' in navigator) {
     (async()=>{
       try{
-        const reg=await navigator.serviceWorker.register('./sw.js?v=37',{updateViaCache:'none'});
+        const reg=await navigator.serviceWorker.register('./sw.js?v=38',{updateViaCache:'none'});
         await reg.update().catch(()=>{});
         if(!window.__swReloadBound){
           window.__swReloadBound=true;
