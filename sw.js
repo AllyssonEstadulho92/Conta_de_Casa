783013a69b8d990caa52f3767a60d39c7d80afd2
AllyssonEstadulho92 <163911342+AllyssonEstadulho92@users.noreply.github.com>
2026-09-06 01:20:08 +0100

    build: renovar cache para v60

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE = 'conta-de-casa-public-v59-product-images';
+const CACHE = 'conta-de-casa-public-v60-retailer-images';
 const PUBLIC_ASSETS = [
   './',
   './index.html',
