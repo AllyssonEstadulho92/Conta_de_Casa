aaba6b06e360106d191383f330afb2d2ee0b2ff5
AllyssonEstadulho92 <163911342+AllyssonEstadulho92@users.noreply.github.com>
2026-09-07 23:16:39 +0100

    build(pwa): refresh cache for v68 menu

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE = 'conta-de-casa-public-v64-runtime1-v65-shopping1-v66-shell1-v67-menu1';
+const CACHE = 'conta-de-casa-public-v64-runtime1-v65-shopping1-v66-shell1-v68-menu2';
 const PUBLIC_ASSETS = [
   './',
   './index.html',
