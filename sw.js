0eec256a2ab1c2803dbdeacb6431f054142e63c6
AllyssonEstadulho92 <163911342+AllyssonEstadulho92@users.noreply.github.com>
2026-09-08 00:45:20 +0100

    v71: atualizar cache público do drawer suave

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE = 'conta-de-casa-public-v64-runtime1-v65-shopping1-v66-shell1-v70-menu4';
+const CACHE = 'conta-de-casa-public-v64-runtime1-v65-shopping1-v66-shell1-v71-menu5';
 const PUBLIC_ASSETS = [
   './',
   './index.html',
