06011f1fddb989b73150ccbc7850adde43e5e4d9
AllyssonEstadulho92 <163911342+AllyssonEstadulho92@users.noreply.github.com>
2026-09-08 15:53:17 +0100

    build(v76): consolidar cache da auditoria final

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE = 'conta-de-casa-public-v76-usability1-a11y1-v75-architecture2-v74-ui1-v74-shopping2-v73-menu8-v74-experience2';
+const CACHE = 'conta-de-casa-public-v76-usability1-v75-architecture2-v74-ui1-v74-shopping2-v73-menu8-v74-experience2';
 const PUBLIC_ASSETS = [
   './',
   './index.html',
