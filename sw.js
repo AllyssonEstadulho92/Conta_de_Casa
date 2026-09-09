1df8eb4f87bf02b57d48d2ff9043d53e0837438b
AllyssonEstadulho92 <163911342+AllyssonEstadulho92@users.noreply.github.com>
2026-09-09 09:26:12 +0100

    build(v75): invalidar cache para layout1

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE = 'conta-de-casa-public-v75-architecture2-v74-ui1-v74-shopping2-v73-menu8-v74-experience2-header2-stability1';
+const CACHE = 'conta-de-casa-public-v75-architecture2-v74-ui1-v74-shopping2-v73-menu8-v74-experience2-header2-stability1-layout1';
 const PUBLIC_ASSETS = [
   './',
   './index.html',
@@ -21,6 +21,7 @@ const PUBLIC_ASSETS = [
   './v75-architecture.css',
   './v75-header-refinement.css',
   './v75-stability.css',
+  './v75-layout-polish.css',
   './core.js',
   './finance.js',
   './render.js',
