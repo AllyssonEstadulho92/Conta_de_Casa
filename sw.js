590fc74711d8b74e21e1817e4f118afb98e98856
AllyssonEstadulho92 <163911342+AllyssonEstadulho92@users.noreply.github.com>
2026-09-09 15:27:33 +0100

    chore(v75): invalidar cache para drawer2 teal

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE = 'conta-de-casa-public-v75-architecture2-v74-ui1-v74-shopping2-v73-menu8-v74-experience2-header2-stability1-layout1-drawer1';
+const CACHE = 'conta-de-casa-public-v75-architecture2-v74-ui1-v74-shopping2-v73-menu8-v74-experience2-header2-stability1-layout1-drawer2';
 const PUBLIC_ASSETS = [
   './',
   './index.html',
@@ -22,7 +22,7 @@ const PUBLIC_ASSETS = [
   './v75-header-refinement.css',
   './v75-stability.css',
   './v75-layout-polish.css',
-  './v75-drawer-blue.css',
+  './v75-drawer-theme.css',
   './core.js',
   './finance.js',
   './render.js',
