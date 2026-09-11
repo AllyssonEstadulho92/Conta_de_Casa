a8a8d1a89d4c56e53b5ca5bd939da44600baa0bf
AllyssonEstadulho92 <163911342+AllyssonEstadulho92@users.noreply.github.com>
2026-09-11 09:34:27 +0100

    release(v76): invalidar cache PWA para a release estável

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const CACHE = 'conta-de-casa-public-v75-architecture2-v74-ui1-v74-shopping2-v73-menu8-v74-experience2-header2-stability1-layout1-drawer2-featured1-image-library1-catalog4-pd-photo1-photo-loader3-startup2-usability1-pages1-assets1-market1-expenses1-veggie-menu2-modern-ui1-version-audit1-mobile-shell2-architecture-baseline1';
+const CACHE = 'conta-de-casa-public-v76-release1-architecture-baseline1-veggie-menu2-modern-ui1-version-audit1-mobile-shell2';
 const NAVIGATION_TIMEOUT_MS = 4000;
 const PUBLIC_ASSETS = [
   './',
